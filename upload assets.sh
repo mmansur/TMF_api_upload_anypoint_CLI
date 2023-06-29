4b8c0d99c44a82d9a5be08a0dbcd6a68cdc167bd
Matheus Mansur Hermsdorff <matheusmansur@gmail.com>
2023-06-29 22:28:27 +0200

    Update upload assets.sh
    Adding resources with diagrams and json sample files

--- upload assets.sh
+++ upload assets.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+
+# Function to split camel case into separate words
+split_camel_case() {
+    echo $1 | sed -r 's/([a-z])([A-Z])/\1 \2/g'
+}
+
+
 # Define your Anypoint Platform login credentials and organization details
-username="user"
-password="pass"
-organization="824821a1-2e2d-4087-93c8-dba3762e8838"
-host="eu1.anypoint.mulesoft.com"
+username=""
+password=""
+organization="-a2ef-41ad-aeef-4b837a72ba43"
+host="anypoint.mulesoft.com"
 
 
 # Log in to Anypoint Platform
@@ -52,11 +59,79 @@ do
                 # Construct the assetIdentifier
                 assetIdentifier="${organization}/${name}/${version}"
 
-                echo "Publishing $name API: $file with version: $version and with assetIdentifier: $assetIdentifier"
                 # Use the Anypoint CLI to publish the API to the exchange
+                echo "Publishing the API to the exchange"
+                echo "Publishing $name API: $file with version: $version and with assetIdentifier: $assetIdentifier"
+                
                 
                 anypoint-cli-v4 exchange:asset:upload --host $host --username $username --password $password --organization $organization --name "$name" --description "$full_detail" --tags "$tag" --type "rest-api"   --properties='{"apiVersion":"'"$version"'", "mainFile": "'"$file"'"}' --files='{"oas.json":"'"$file"'"}' $assetIdentifier
-                
+
+
+                echo "$(pwd)"
+
+                # Navigate to the "samples" subfolder and create a page for each sample file
+                if [ -d "../samples" ]; then
+                    cd "../samples"
+                    for sample in *.example.json; do
+                        # Generate a proper name for the sample
+                        sample_name=${sample%%.example.json*}
+                        sample_name=$(split_camel_case "$sample_name")
+                        # Create a page for the sample in Anypoint Exchange
+                        echo "Creating a page for the sample in Anypoint Exchange"
+                        echo "Sample name $sample_name  Filename $sample"  
+                        
+
+                        anypoint-cli-v4 exchange:asset:page:upload --host $host --username $username --password $password --organization $organization $assetIdentifier "JSON Example $sample_name" $sample
+
+                    done
+                fi        
+                echo "$(pwd)"
+                cd ../swaggers
+                echo "$(pwd)"
+
+                # Navigate to the "documentation/diagrams" subfolder, upload each PNG file as a resource, and create a page for each resource
+                if [ -d "../documentation/diagrams" ]; then
+                    cd "../documentation/diagrams"
+                    for diagram in *.png; do
+                        # Extract the diagram name and format it
+                        diagram_name=${diagram#Resource_}; diagram_name=${diagram_name%%.png}
+                        readable_diagram=$(echo $diagram_name | sed -r 's/([a-z])([A-Z])/\1 \2/g')
+                        readable_diagram=$(echo "$readable_diagram" | tr -d '%@*+/_\\')
+
+
+
+                        echo "Resource name $readable_diagram"
+                        # Upload the PNG file as a resource in Anypoint Exchange
+                        
+                        # Upload the diagram as a resource and capture the output
+                        echo "Uploading the diagram as a resource and capture the output"
+                        upload_output=$(anypoint-cli-v4 exchange:asset:resource:upload --host $host --username $username --password $password --organization $organization $assetIdentifier $diagram)
+
+                        echo "Result from the resource upload: $upload_output"
+
+                        # Extract the last line starting with "![Resource_Alarm]" from the output and store it in a variable
+                        last_line=$(echo "$upload_output" | tail -n 1)
+                        echo "Link to the resource: $last_line"
+
+                        # Create a temporary file
+                        temp_file=$(mktemp)
+
+                        # Write the content of the variable "resource_markdown_code" to the temporary file
+                        echo "$last_line" > "$temp_file"
+
+                        # Print the name of the temporary file
+                        echo "The temporary file is located at: $temp_file"
+
+                        # Create a page for the resource in Anypoint Exchange
+                        echo "Creating a page for the resource in Anypoint Exchange"
+                        anypoint-cli-v4 exchange:asset:page:upload --host $host --username $username --password $password --organization $organization $assetIdentifier "Diagram $readable_diagram" $temp_file
+
+                        # Delete the temporary file
+                        #rm "$temp_file"
+                    done
+                    cd ../../swaggers
+                fi
+                echo "$(pwd)"
             done
         else
             echo "No YAML files found in $dir/swaggers."
